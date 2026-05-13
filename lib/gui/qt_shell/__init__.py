21c18a66273af19ddab8d560815004c18b903db4
Drew Giacomi <andrew.giacomi@gmail.com>
2026-05-13 08:17:53 -0400

    Enable Qt preview lifecycle hooks

--- lib/gui/qt_shell/__init__.py
+++ lib/gui/qt_shell/__init__.py
@@ -3,5 +3,8 @@
 
 from lib.gui.qt_shell.main import main
 from lib.gui.qt_shell.main_window import MainWindow
+from lib.gui.qt_shell.preview_job_lifecycle import install_preview_job_lifecycle as _install_hooks
+
+_install_hooks(MainWindow)
 
 __all__ = ["MainWindow", "main"]
