682799bf593956dd1a91c4570f7664f9e63f62fa
Drew Giacomi <andrew.giacomi@gmail.com>
2026-05-12 21:02:29 -0400

    Add Qt graph panel file

---
+++ lib/gui/qt_shell/graph_panel.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Qt graph panel."""
+
+from __future__ import annotations
+
+from PySide6.QtWidgets import QWidget
+
+
+class GraphPanel(QWidget):
+    """Runtime graph panel."""
+
+    pass
