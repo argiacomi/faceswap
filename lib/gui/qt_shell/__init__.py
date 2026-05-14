1bf3da3342a3236ee0fe90a9caf2b8841da2a9f8
Drew Giacomi <andrew.giacomi@gmail.com>
2026-05-13 20:08:16 -0400

    Install Qt recent menu display adapter

--- lib/gui/qt_shell/__init__.py
+++ lib/gui/qt_shell/__init__.py
@@ -4,7 +4,9 @@
 from lib.gui.qt_shell.main import main
 from lib.gui.qt_shell.main_window import MainWindow
 from lib.gui.qt_shell.preview_job_lifecycle import install_preview_job_lifecycle as _install_preview_job_lifecycle
+from lib.gui.qt_shell.recent_menu import install_recent_menu_display as _install_recent_menu_display
 
 _install_preview_job_lifecycle(MainWindow)
+_install_recent_menu_display(MainWindow)
 
 __all__ = ["MainWindow", "main"]
