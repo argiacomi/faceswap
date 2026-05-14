c4ab86bc3b9038c1236b7ccc6ddd36a9cb163426
Drew Giacomi <andrew.giacomi@gmail.com>
2026-05-14 01:32:14 -0400

    Install Qt settings menu adapter

--- lib/gui/qt_shell/__init__.py
+++ lib/gui/qt_shell/__init__.py
@@ -3,5 +3,8 @@
 
 from lib.gui.qt_shell.main import main
 from lib.gui.qt_shell.main_window import MainWindow
+from lib.gui.qt_shell.settings_menu import install_settings_menu as _install_settings_menu
+
+_install_settings_menu(MainWindow)
 
 __all__ = ["MainWindow", "main"]
