1c46cc4431eaa7a1d80da126a32d25395caa95fc
Drew Giacomi <andrew.giacomi@gmail.com>
2026-05-13 20:21:49 -0400

    Install command option state adapter

--- lib/gui/qt_shell/__init__.py
+++ lib/gui/qt_shell/__init__.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 """Prototype Qt shell for validating the Faceswap GUI service layer."""
 
+from lib.gui.qt_shell.command_option_state import install_command_option_state as _install_command_option_state
+from lib.gui.qt_shell.command_panel import CommandPanel
 from lib.gui.qt_shell.main import main
 from lib.gui.qt_shell.main_window import MainWindow
+
+_install_command_option_state(CommandPanel)
 
 __all__ = ["MainWindow", "main"]
