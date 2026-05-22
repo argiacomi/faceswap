fddb48dbb32a94a3e0f1903c983c9f2391bd14ad
Drew Giacomi <andrew.giacomi@gmail.com>
2026-05-22 18:33:57 -0400

    Install Qt Manual Tool thumbnail regeneration shim

---
+++ tools/manual/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Manual Tool package helpers."""
+
+from .qt_thumbnail_regeneration import install as _install_qt_thumbnail_regeneration
+
+_install_qt_thumbnail_regeneration()
+
+__all__ = []
